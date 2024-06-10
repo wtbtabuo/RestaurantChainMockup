c30c41a5883b91143809e2be44f0a8ef2be50a4d
Keita Tabuchi <keita.tabuchi@firstloop-tech.com>
2024-06-10 09:27:22 +0900

    fix: change files into src directory

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use RestaurantChain\RestaurantChain;
 
+$min = $_GET['min'] ?? 2;
+$max = $_GET['max'] ?? 5;
+
+$min = (int)$min;
+$max = (int)$max;
+
 // サンプルデータを生成
 $restaurantChains = [
     RestaurantChain::RandomGenerator(),
